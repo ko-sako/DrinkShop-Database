b8099ccaed156c14f57b26c33dd1ce621d6f5d81
Kayo Osako <osakok@cardiff.ac.uk>
2025-02-10 11:00:22 +0000

    change the order of statements for refering foreign key correctly

--- create_schema_cardiff_drinks(3).sql
+++ create_schema_cardiff_drinks(3).sql
@@ -1,4 +1,31 @@
 DROP TABLE IF EXISTS online_activity;
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS regions;
+CREATE TABLE regions (
+     region_id int NOT NULL AUTO_INCREMENT,
+     name varchar(50),
+     CONSTRAINT regions_PK PRIMARY KEY (region_id)
+);
+
+CREATE TABLE salespersons (
+      salesperson_id int NOT NULL AUTO_INCREMENT,
+      name varchar(75),
+      region_id int,
+      CONSTRAINT salespersons_PK PRIMARY KEY (salesperson_id),
+      CONSTRAINT FK_regions FOREIGN KEY (region_id) REFERENCES regions(region_id)  -- PK and FK must be of the same data type and of the same size!
+);
+
+CREATE TABLE clients (
+     client_id int NOT NULL AUTO_INCREMENT,
+     name varchar(100),
+     website varchar(50),
+     flagship_poc varchar(50),
+     salesperson_id int,
+     CONSTRAINT clients_PK PRIMARY KEY (client_id),
+     CONSTRAINT FK_salespersons FOREIGN KEY (salesperson_id) REFERENCES salespersons(salesperson_id)  -- PK and FK must be of the same data type and of the same size!
+);
+
 CREATE TABLE online_activity (
 	activity_id int NOT NULL AUTO_INCREMENT,
 	client_id int,
@@ -6,24 +33,6 @@ CREATE TABLE online_activity (
 	platform varchar(50),
 	CONSTRAINT online_activity_PK PRIMARY KEY (activity_id),
 	CONSTRAINT FK_online_activity_clients FOREIGN KEY (client_id) REFERENCES clients(client_id)  -- PK and FK must be of the same data type and of the same size!
-);
-
-DROP TABLE IF EXISTS regions;
-CREATE TABLE regions (
-	region_id int NOT NULL AUTO_INCREMENT,
-	name varchar(50),
-	CONSTRAINT regions_PK PRIMARY KEY (region_id)
-);
-
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
-	client_id int NOT NULL AUTO_INCREMENT,
-	name varchar(100),
-	website varchar(50),
-	flagship_poc varchar(50),
-	salesperson_id int,
-	CONSTRAINT clients_PK PRIMARY KEY (client_id),
-	CONSTRAINT FK_salespersons FOREIGN KEY (salesperson_id) REFERENCES salespersons(salesperson_id)  -- PK and FK must be of the same data type and of the same size!
 );
 
 DROP TABLE IF EXISTS placed_orders;
@@ -44,12 +53,5 @@ CREATE TABLE placed_orders (
 );
 
 
-DROP TABLE IF EXISTS salespersons;
-CREATE TABLE salespersons (
-	salesperson_id int NOT NULL AUTO_INCREMENT,
-	name varchar(75),
-	region_id int,
-	CONSTRAINT salespersons_PK PRIMARY KEY (salesperson_id),
-	CONSTRAINT FK_regions FOREIGN KEY (region_id) REFERENCES regions(region_id)  -- PK and FK must be of the same data type and of the same size!
-);
+
 
